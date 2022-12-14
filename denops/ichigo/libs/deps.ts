0f89478a6742dc0da402de1ef734ef541a98e6ba
denopendabot <denopendabot@github.com>
2022-12-14 15:11:59 +0000

    build(deps): bump deno.land/x/denops_std from v3.11.3 to v3.12.0

--- denops/ichigo/libs/deps.ts
+++ denops/ichigo/libs/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.11.3/mod.ts";
-export { filetype } from "https://deno.land/x/denops_std@v3.11.3/option/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.11.3/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
+export { filetype } from "https://deno.land/x/denops_std@v3.12.0/option/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts";
 export { open } from "https://deno.land/x/open@v0.0.5/index.ts";
 export { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 export { getFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
